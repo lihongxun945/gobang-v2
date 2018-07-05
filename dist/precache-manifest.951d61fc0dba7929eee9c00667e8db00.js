d8b7d5335b9bda7fefe316b143122c28d5d8826a
lihongxun945 <lihongxun945@gmail.com>
2018-07-05 22:43:12 +0800

    build

---
+++ dist/precache-manifest.951d61fc0dba7929eee9c00667e8db00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "eab35b2ddb9142b534c4",
+    "url": "/js/chunk-vendors.eab35b2d.js"
+  },
+  {
+    "revision": "21e7118a4af68c2eb23b95fe5ed5f873",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5442f90f0a7d84ccc30d008200e3aee6",
+    "url": "/img/board.5442f90f.jpg"
+  },
+  {
+    "revision": "eab35b2ddb9142b534c4",
+    "url": "/css/chunk-vendors.bfa4af2a.css"
+  },
+  {
+    "revision": "b93d3a47dd65f6c46341",
+    "url": "/css/app.0ec3735d.css"
+  },
+  {
+    "revision": "b93d3a47dd65f6c46341",
+    "url": "/app.bundle.js"
+  },
+  {
+    "revision": "555cf1f33246d6ad43a8",
+    "url": "/ai.bundle.js"
+  }
+];
